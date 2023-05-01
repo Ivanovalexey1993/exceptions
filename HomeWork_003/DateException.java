5425dfe7e65e7aec2a900c0d68538282f2fe96c0
ivanovalexey1993 <ivanovalexey1993@mail.ru>
2023-05-01 09:25:52 +0300

    SEMINAR 3

---
+++ HomeWork_003/DateException.java
@@ -0,0 +1,11 @@
+package HomeWork_003;
+
+public class DateException {
+    
+}
+public void dataException(String i) {
+    System.out.println("Exception: DateNotCorrectFormat");
+    System.out.printf("It`s not correct date format: %s", i);
+    System.out.println();
+}
+}
